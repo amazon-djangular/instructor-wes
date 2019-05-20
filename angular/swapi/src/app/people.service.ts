9c1850136ca324245fafeaf8523b91706082de8b
Weston Harper <wes@tao.team>
2019-05-20 17:27:15 -0600

    removed unnecessary import

--- angular/swapi/src/app/people.service.ts
+++ angular/swapi/src/app/people.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { People } from './people';
 
 @Injectable({
